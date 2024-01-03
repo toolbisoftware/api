720e3972a91f74b55baaa53d51dc79d5de9ef581
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-03 17:31:38 +0100

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,33 @@
 // Check the README file in the project root for more information.
 
 export type LoggerLogCategories = "api";
+
+//
+
+export type Modes = "development" | "production";
+
+//
+
+export interface ConfigMode {
+  host: string;
+  port: number;
+  url: string;
+  databaseHost: string;
+  databasePort: number;
+  databaseUser: string;
+  databasePassword: string;
+  databaseDatabase: string;
+  keystoreHost: string;
+  keystorePort: number;
+  keystoreUser: string;
+  keystorePassword: string;
+  keystoreDatabase: number;
+}
+
+export interface ConfigShared {}
+
+export type Config = {
+  [K in Modes as K]: ConfigMode;
+} & {
+  shared: ConfigShared;
+};
