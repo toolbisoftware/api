ee97bee72c7ab3a87b3c75d43c7b09d24882051c
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-03 14:11:17 +0100

    Create production.template.ts

---
+++ src/secrets/production.template.ts
@@ -0,0 +1,68 @@
+// Copyright (c) Toolbi Software. All rights reserved.
+// Check the README file in the project root for more information.
+
+/**
+ * @example
+ * "https://api.toolbi.xyz"
+ */
+export const HOST = "";
+/**
+ * @example
+ * 4000
+ */
+export const PORT = -1;
+/**
+ * @example
+ * "https://api.toolbi.xyz"
+ */
+export const URL = "";
+/**
+ * @example
+ * "127.0.0.1"
+ */
+export const DATABASE_HOST = "";
+/**
+ * @example
+ * 5000
+ */
+export const DATABASE_PORT = -1;
+/**
+ * @example
+ * "postgres"
+ */
+export const DATABASE_USER = "";
+/**
+ * @example
+ * "hh3jNQjAPLx2USIWPvm86tocRppFwieUFZNykyZdTsDDN7JqT6ZLfeMUm3dj6iWFHUnxiUXvnyiJqQwY28hACdF55XQnXu7POmudt7coFbmG8ZKvOCZ4s3vgeksw3TSTwra4U5NjkWuifzSXXIivgui84OZXtpl7N8ju7x2CUdRXl53LZP4bwKXWsIK5F2MaGS5NoZAYk8dugfJXFcCEeZzjAMJixyRnUEWBvzfVz775E7QoxDrrpVVwfgxdyNwxDjENWnxhBicQAVe744NtzjGgCY5mRAAAT8o7quIeOSORjcME8U82AGEkH7cUKcTagIYCj724kjYAiJNvOaAlzTfGoOVpvESIOgyoVY2XjmQN6zUzrWt8MtiSMjjiCl8aoVmbNWvp7kRugrumfKPw8XyL4wMpNlGXFFKjudbpBn8z2t7GAMrxWPDLseBUxwVnXsdxi5lZtRX6ay2QadkBQLaIGKsH3l4ODJahXWwBQhwEmaOaiZSIFZcx22ztyxPm8Smr3XUFoCw5fgH582XLjsqIF8lGOm6xJ2dI3T84OFa3T4rBecOTZvsKpiPW7wGBMKlu2srdvlZZGnkouY7DYnSi8ls22RH5wu8hOXNsjZBRFwGTU8hxA2ZoskpXP4X8HfI55pCafBLGg3cjTo4LpCtvJlw7PlM8UDfhD5fahApk5E7VnRgdVMXqdMnKEpdJfIyLyInOQmzGaAgNmSga7CbGJ2IYAWJuZPaoJYMngscjRq8vXjHi55ANCP6Zvc8sJRHgUWWuyixPcsOB7WkvVtediCMA8giCGHffE35zyCCsXfrlGqeNxhJOlDZPUYMZER66gb8VETaSKV8RwHb7a8iCtbHx4kyX8qOjRVIGiUAincoJLps7bsyYMUel2LsNquNCHLbW4jG6dHqFRf4fBWEgc8CqRsP8zkeeMzgTN8ebO6VAnrM8g4pZAHOKTJvmJKHoIqnmXXYfawXk82FL4rkAEjROX3pK4cxQge4zCYJu23tX6eVxPnwGu6ouaroK"
+ */
+export const DATABASE_PASSWORD = "";
+/**
+ * @example
+ * "postgres"
+ */
+export const DATABASE_DATABASE = "";
+/**
+ * @example
+ * "127.0.0.1"
+ */
+export const KEYSTORE_HOST = "";
+/**
+ * @example
+ * 5100
+ */
+export const KEYSTORE_PORT = -1;
+/**
+ * @example
+ * "default"
+ */
+export const KEYSTORE_USER = "";
+/**
+ * @example
+ * "jtQxb75G8tpZdOoQPJze5J8khxNPKXw6NOvGUeTv837jUQ77r7ZrnnJjwWGBflS4SSsGiRHx36b5QGN7qCLDgNDFGQDezBfFIVeaphSyw4IxRUUZRysdJdTfSMEgvbcUTFGPsMih7bq3Ark467YMgzMmA8bE6tWzH3Ice4WzMeA4RVlGSGQrmUSRxXlccJoZENKuq5zTGmnoGPtMAECJF7rRJbcnNOB8H6U5oXxCHpgKwvslxecF7jglaQ3KiMlPQfNkmHRvf2sALWhHwtZDcWEgNYWt6Hz6BkvdmFeUOJPKwEij2vfdbC78uBxmNbBzZGFHzxB32gZIReB7PP5ZxCzQ5RyZa6y2pnQZK2BIECTITfOVriyclYR2vnXo8hvYkLrlzuEEHPuiCnjoa3e6Jn2l5gOE3enCicY8VYsM4HxKVHZRJEBqe8fyUUULYRXGaSalx6SRucME5hmLtRdzFbQrEHYt3jwH6gfHwo6ZkTuOCCwvcoWRBLqFgeooJqZTG8oUfZHIVwQs2Kj2q7d7VdMLo2CY8LPaL7DIcErlNlaQRuWt6Weo3pvxqSwHgf6Vuk7dWDFGvAyRTHZjxtCLahq3IMTGuS7kDAMepiCA2lO7x5pSBGzYBpNXdMv3cjoXq26RMVcAcaXbeNGQJsCdDCrBrM4b2ZZ7M8raVrqqnagO8z4lXQNj3QYNwNvtEYNuGf2wkM63aA6tAxpSUQLdx3xaEMaTJYuwOC6MvviwZDw2qZotJvuMGaD3YyPfNAMLXIaZznulNZIboQaW82YfWjRveNpwzObqIG3XNISLrpiHUMuC6rBgUqFFS82zaxx5UweyXIvDUOhhhrcYmxfP7wjYwlk5utQxC5EOCLBmfNbxgpTx4Q4UUOiHsqU7uPqZo5NouDqB6dvkq6QXNrQRpMW8KMoMjplzN54fQI8KB8zpEekClDNLONUKhB5oKPr5FgQ4SQF8nzozWHvdCFtp4G783k38SeFJTArzpdcvTi7Nq2twPZRmG6KE36EMgKgF"
+ */
+export const KEYSTORE_PASSWORD = "";
+/**
+ * @example
+ * 0
+ */
+export const KEYSTORE_DATABASE = "";
