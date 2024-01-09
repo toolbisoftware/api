b87d01cd576593b18f96f6e90ecc4b428890c4c5
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-09 18:22:10 +0100

    Update index_.ts

--- src/routes/index_.ts
+++ src/routes/index_.ts
@@ -2,16 +2,25 @@
 // Check the README file in the project root for more information.
 
 import API from "../api/index.js";
-import type { RouteMethods, RouteOptions } from "../types.js";
+import type {
+  RouteMethods,
+  RouteOptions,
+  RouteOptionsCooldown,
+  RouteOptionsRatelimit
+} from "../types.js";
 
 export class Route {
   readonly api: API;
   readonly enabled: boolean;
+  readonly ratelimit: RouteOptionsRatelimit;
+  readonly cooldown: RouteOptionsCooldown;
   readonly methods: RouteMethods;
 
   constructor(api: API, options: RouteOptions, methods: RouteMethods) {
     this.api = api;
     this.enabled = options.enabled;
+    this.ratelimit = options.ratelimit;
+    this.cooldown = options.cooldown;
     this.methods = methods;
   }
 }
