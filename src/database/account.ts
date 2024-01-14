462a94ab2183a54431443e8d2646f48bff953012
NEXZ <76069669+soynexz@users.noreply.github.com>
2024-01-14 16:30:58 +0100

    Update account.ts

--- src/database/account.ts
+++ src/database/account.ts
@@ -17,31 +17,31 @@ export default class Account {
       await this.#api.database.client.query(`
         CREATE TABLE IF NOT EXISTS "account" (
           "id" SERIAL PRIMARY KEY,
-          "information_name" VARCHAR(30) NOT NULL,
-          "information_birthDate" DATE NOT NULL,
-          "information_username" VARCHAR(30) NOT NULL,
-          "security_emailAddress" VARCHAR(320) NOT NULL,
-          "security_password" VARCHAR(256) NOT NULL,
-          "timestamp_creation" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-          "timestamp_deletion" TIMESTAMP
+          "name" VARCHAR(30) NOT NULL,
+          "birth_date" DATE NOT NULL,
+          "username" VARCHAR(30) NOT NULL,
+          "email_address" VARCHAR(320) NOT NULL,
+          "password" VARCHAR(256) NOT NULL,
+          "creation_date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          "deletion_date" TIMESTAMP
         )`);
       await this.#api.database.client.query(`
         CREATE TABLE IF NOT EXISTS "account_role" (
           "id" SERIAL PRIMARY KEY,
           "name" VARCHAR(100) NOT NULL,
-          "timestamp_creation" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-          "timestamp_deletion" TIMESTAMP
+          "creation_date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          "deletion_date" TIMESTAMP
         )`);
       await this.#api.database.client.query(`
         CREATE TABLE IF NOT EXISTS "account_log" (
-          "id" UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+          "id" UUID DEFAULT gen_random_uuid(),
           "date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
           "account_id" INTEGER REFERENCES account(id) ON DELETE CASCADE NOT NULL,
           "type" VARCHAR(30) NOT NULL,
           "content" VARCHAR(512) NOT NULL
         )`);
       await this.#api.database.client.query(`
-        CREATE TABLE IF NOT EXISTS "account_role_data" (
+        CREATE TABLE IF NOT EXISTS "account_roles" (
           "account_id" INTEGER REFERENCES account(id) ON DELETE CASCADE NOT NULL,
           "role_id" INTEGER REFERENCES account_role(id) ON DELETE CASCADE NOT NULL,
           PRIMARY KEY (account_id, role_id)
